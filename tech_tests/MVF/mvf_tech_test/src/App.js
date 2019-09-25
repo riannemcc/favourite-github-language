226bf6c2628b968f85ff25a5e62d6f1fffe65660
Rianne McCartney <riannemccartney@hotmail.com>
2019-09-25 22:40:10 +0100

    App now returns list of all languages used in repo

--- tech_tests/MVF/mvf_tech_test/src/App.js
+++ tech_tests/MVF/mvf_tech_test/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import GetUserInformation from './GetUserInformation';
-// import Repos from './Repos';
-import axios from 'axios'
+
+
 
 class App extends Component {
   constructor () {
--- tech_tests/MVF/mvf_tech_test/src/GetUserInformation.js
+++ tech_tests/MVF/mvf_tech_test/src/GetUserInformation.js
@@ -10,11 +10,12 @@ class GetUserInformation extends React.Component {
         value: '',
         username: '',
         avatar: '',
-        repos: ''
+        repoLanguages: []
       };
      
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.getLanguages = this.getLanguages.bind(this);
     }
   
     handleChange(event) {
@@ -22,38 +23,42 @@ class GetUserInformation extends React.Component {
     }
   
     handleSubmit(event) {
-    let name = this.state.value;
-    let url = "https://api.github.com/users/" + name;
-    let repos_url = "https://api.github.com/users/" + name + "/repos"
+      let name = this.state.value;
+      let url = "https://api.github.com/users/" + name;
+      let repos_url = "https://api.github.com/users/" + name + "/repos"
       alert('You have searched for: ' + this.state.value);
       event.preventDefault();
       axios.get(url)
-      .then(response => {
-        this.setState({username: response.data.name})
-        this.setState({avatar: response.data.avatar_url})
-        return axios.get(repos_url);
-      })
-      .then(response => {
-        this.setState({repos: response.data[0].full_name})
-      })
-      .catch(error => {
-        console.log(error)
-      });
+        .then(response => {
+          this.setState({username: response.data.name})
+          this.setState({avatar: response.data.avatar_url})
+          return axios.get(repos_url);
+        })
+        .then(response => {
+          this.setState({repoLanguages: this.getLanguages(response.data)})
+        })
+        .catch(error => {
+          console.log(error)
+        });
     }
-    
+
+    getLanguages(repos) {
+      return repos.map((element) => { 
+        return element.language });
+    }
+
     render() {
+      console.log(this.state.repoLanguages)
       return (
         <div>
-
             <form onSubmit={this.handleSubmit}>
                 <input type="text" placeholder="Enter GitHub username" value={this.state.value} onChange={this.handleChange} />
-            <div className='button__container'>
-              <input className='button' type="submit" value="Submit" />
-              <p>{this.state.username}</p>
-              <img src={this.state.avatar} alt="Profile avatar"></img>
-              <p>{this.state.repos}</p>
-            </div>
-        
+              <div className='button__container'>
+                <input className='button' type="submit" value="Submit" />
+                <h1>{this.state.username}</h1>
+                <img src={this.state.avatar} alt="Profile avatar"></img>
+                <li>{this.state.repoLanguages} </li>
+              </div>
             </form>
         </div>
       );
